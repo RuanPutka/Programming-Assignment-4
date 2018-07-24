570186b9bc7c07dd61ec5b640ec1f2fa2a846deb
Ruan Putka <ruan.fp@gmail.com>
2018-07-24 15:35:53 -0300

    First commit.

---
+++ Codebook.md
@@ -0,0 +1,99 @@
+# Codebook
+
+## Identifiers
+
+* `Subjects`: The ID of the test subject.
+* `ActivityLevel`: Activity performed when the measurements were taken.
+
+## Activity Levels
+
+ Factor describing the subject activity during the test.
+ 
+1. `WALKING`
+2. `WALKING_UPSTAIRS`
+3. `WALKING_DOWNSTAIRS`
+4. `SITTING`
+5. `STANDING`
+6. `LAYING`
+
+## Measurements
+
+* `tBodyAccMeanX`
+* `tBodyAccMeanY`
+* `tBodyAccMeanZ`
+* `tBodyAccStdX`
+* `tBodyAccStdY`
+* `tBodyAccStdZ`
+* `tGravityAccMeanX`
+* `tGravityAccMeanY`
+* `tGravityAccMeanZ`
+* `tGravityAccStdX`
+* `tGravityAccStdY`
+* `tGravityAccStdZ`
+* `tBodyAccJerkMeanX`
+* `tBodyAccJerkMeanY`
+* `tBodyAccJerkMeanZ`
+* `tBodyAccJerkStdX`
+* `tBodyAccJerkStdY`
+* `tBodyAccJerkStdZ`
+* `tBodyGyroMeanX`
+* `tBodyGyroMeanY`
+* `tBodyGyroMeanZ`
+* `tBodyGyroStdX`
+* `tBodyGyroStdY`
+* `tBodyGyroStdZ`
+* `tBodyGyroJerkMeanX`
+* `tBodyGyroJerkMeanY`
+* `tBodyGyroJerkMeanZ`
+* `tBodyGyroJerkStdX`
+* `tBodyGyroJerkStdY`
+* `tBodyGyroJerkStdZ`
+* `tBodyAccMagMean`
+* `tBodyAccMagStd`
+* `tGravityAccMagMean`
+* `tGravityAccMagStd`
+* `tBodyAccJerkMagMean`
+* `tBodyAccJerkMagStd`
+* `tBodyGyroMagMean`
+* `tBodyGyroMagStd`
+* `tBodyGyroJerkMagMean`
+* `tBodyGyroJerkMagStd`
+* `fBodyAccMeanX`
+* `fBodyAccMeanY`
+* `fBodyAccMeanZ`
+* `fBodyAccStdX`
+* `fBodyAccStdY`
+* `fBodyAccStdZ`
+* `fBodyAccMeanFreqX`
+* `fBodyAccMeanFreqY`
+* `fBodyAccMeanFreqZ`
+* `fBodyAccJerkMeanX`
+* `fBodyAccJerkMeanY`
+* `fBodyAccJerkMeanZ`
+* `fBodyAccJerkStdX`
+* `fBodyAccJerkStdY`
+* `fBodyAccJerkStdZ`
+* `fBodyAccJerkMeanFreqX`
+* `fBodyAccJerkMeanFreqY`
+* `fBodyAccJerkMeanFreqZ`
+* `fBodyGyroMeanX`
+* `fBodyGyroMeanY`
+* `fBodyGyroMeanZ`
+* `fBodyGyroStdX`
+* `fBodyGyroStdY`
+* `fBodyGyroStdZ`
+* `fBodyGyroMeanFreqX`
+* `fBodyGyroMeanFreqY`
+* `fBodyGyroMeanFreqZ`
+* `fBodyAccMagMean`
+* `fBodyAccMagStd`
+* `fBodyAccMagMeanFreq`
+* `fBodyBodyAccJerkMagMean`
+* `fBodyBodyAccJerkMagStd`
+* `fBodyBodyAccJerkMagMeanFreq`
+* `fBodyBodyGyroMagMean`
+* `fBodyBodyGyroMagStd`
+* `fBodyBodyGyroMagMeanFreq`
+* `fBodyBodyGyroJerkMagMean`
+* `fBodyBodyGyroJerkMagStd`
+* `fBodyBodyGyroJerkMagMeanFreq`
---
+++ run_analysis.R
@@ -0,0 +1,48 @@
+library(dplyr)
+library(reshape)
+
+setwd("~/R Projects/R Programming/Programming Assignment 4")
+
+## Reads a 561-feature vector with time and frequency domain variables, getting
+## the features names to use as columns in the training and test dataset.
+features.vector <- read.table("UCI HAR Dataset//features.txt")
+features.names <- as.character(features.vector[,2])
+
+## Merges the training and the test sets to create one dataset.
+df <- rbind(read.table("UCI HAR Dataset//train//X_train.txt"),
+           read.table("UCI HAR Dataset//test//X_test.txt"))
+
+## Extracts only the measurements of the mean and standard deviation
+## for each observation.
+names(df) <- features.names
+new.names <- features.names[grepl("[Ss][Tt][Dd]|[Mm][Ee][Aa][Nn]",
+                                     features.names)]
+df <- df[,new.names]
+
+## Setting descriptive activity names to name the activities in the dataset.
+activity.levels <- c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", 
+                            "SITTING", "STANDING", "LAYING")
+labels <- rbind(read.table("UCI HAR Dataset//train//y_train.txt"),
+                read.table("UCI HAR Dataset//test//y_test.txt"))
+labels <- sapply(labels, function(e) activity.levels[e])
+
+## Merges df, labels and subjects.
+subjects <- rbind(read.table("UCI HAR Dataset//train//subject_train.txt"),
+                  read.table("UCI HAR Dataset//test//subject_test.txt"))
+df <- cbind(df, labels, subjects)
+
+## Appropriately labels the data set with descriptive variable names
+new.names = gsub('-[Mm]ean*', 'Mean', new.names)
+new.names = gsub('-[Ss]td*', 'Std', new.names)
+new.names <- gsub('[-()]', '', new.names)
+
+names(df) <- c(new.names, "ActivityLevels", "Subjects")
+
+## Turn activities and subjects into factors.
+df$ActivityLevels <- as.factor(df$ActivityLevels)
+df$Subjects <- as.factor(df$Subjects)
+
+## Creates a second, independent tidy data set with the average of each 
+## variable for each activity and each subject.
+df.melted <- melt(df, id = c("ActivityLevels", "Subjects"))
+df.tiny <- cast(df.melted, Subjects + ActivityLevels ~ variable, mean)
